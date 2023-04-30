308d54412f8d6ad7eeb9959a568355d9ad1add49
Geraldine Koh <geraldine.jx@gmail.com>
2023-05-01 01:41:12 +0800

    working

--- app/src/main/java/com/example/madpractical2/MainActivity.java
+++ app/src/main/java/com/example/madpractical2/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     final String TITLE = "Main Activity";
+    User myUser = new User();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,55 +17,68 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.v(TITLE, "On Create!");
 
-        User myUser = new User();
-        String name = myUser.getName();
-        String description = myUser.getDescription();
-        final Boolean[] followed = {myUser.getFollowed()};
+        // Restore the followed state from savedInstanceState, if available
+        if (savedInstanceState != null) {
+            myUser = (User) savedInstanceState.get("myUser");
+        }
 
         // Set the text of the follow button based on the "followed" variable
-        final Button[] followButton = {findViewById(R.id.button2)};
+        final Button followButton = findViewById(R.id.button2);
+        if (myUser.getFollowed()) {
+            followButton.setText("Unfollow");
+        } else {
+            followButton.setText("Follow");
+        }
 
-        followButton[0].setOnClickListener(new View.OnClickListener() {
+        followButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.v(TITLE, "Follow button is pressed");
-                if (followed[0] ==false) {
-                    followButton[0].setText("Unfollow");
-                    followed[0] = true;
+                if (followButton.getText().equals("Follow")) {
+                    followButton.setText("Unfollow");
+                    myUser.setFollowed(true);
                 } else {
-                    followButton[0].setText("Follow");
-                    followed[0] = false;
+                    followButton.setText("Follow");
+                    myUser.setFollowed(false);
                 }
             }
         });
     }
 
     @Override
-    protected void onStart(){
+    protected void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putSerializable("myUser", myUser);
+        super.onSaveInstanceState(savedInstanceState);
+        Log.v(TITLE, "On SaveInstanceState!");
+    }
+
+    @Override
+    protected void onStart() {
         super.onStart();
         Log.v(TITLE, "On Start!");
     }
 
     @Override
-    protected void onPause(){
+    protected void onPause() {
         super.onPause();
         Log.v(TITLE, "On Pause!");
     }
 
     @Override
-    protected void onResume(){
+    protected void onResume() {
         super.onResume();
         Log.v(TITLE, "On Resume!");
 
     }
 
     @Override
-    protected void onStop(){
+    protected void onStop() {
         super.onStop();
         Log.v(TITLE, "On Stop!");
     }
+
     @Override
-    protected void onDestroy(){
+    protected void onDestroy() {
         super.onDestroy();
         Log.v(TITLE, "On Destroy!");
     }
